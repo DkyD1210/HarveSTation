6a9fd383ecb71dfbc5647d44bbb00a19c4f2e86a
DkyD1210 <harang0512@naver.com>
2023-06-13 19:20:15 +0900

    동기화용 파일

---
+++ Assets/Main/Script/Player.cs
@@ -0,0 +1,28 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    [SerializeField]
+    private float m_Speed;
+
+
+
+    void Start()
+    {
+        
+    }
+
+    
+
+    void Update()
+    {
+        PlayerMove();
+    }
+
+    private void PlayerMove()
+    {
+
+    }
+}
